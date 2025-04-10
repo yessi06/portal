51eb64c7cb5bcc121fb263436b9ad9208e6625d4
Yessi <yessicac187@gmail.com>
2025-04-10 11:57:09 -0500

    conexion base de datos

--- backoffice/modelos/conexion.php
+++ backoffice/modelos/conexion.php
@@ -2,14 +2,28 @@
 
 class Conexion
 {
+	static private function getEnv($key)
+	{
+		$envPath = __DIR__ . '/../../.env.php';
+		if (file_exists($envPath)) {
+			$env = include($envPath);
+			return $env[$key] ?? null;
+		}
+		return null;
+	}
 
 	static public function conectar()
 	{
 		try {
+			$host = self::getEnv('DB_HOST');
+			$dbname = self::getEnv('DB_NAME');
+			$user = self::getEnv('DB_USER');
+			$pass = self::getEnv('DB_PASS');
+
 			$link = new PDO(
-				"mysql:host=localhost;dbname=crm",
-				"root",
-				"",
+				"mysql:host=$host;dbname=$dbname",
+				$user,
+				$pass,
 				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
 			);
 
