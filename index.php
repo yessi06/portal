3db787e69f955cd1cd879fd0a50993116b3a0b14
Yessi <yessicac187@gmail.com>
2025-04-10 19:30:19 -0500

    local

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once "controladores/plantilla.controlador.php";
 require_once "controladores/ruta.controlador.php";
